f7afeb81064e7e43ddd0f67c4ad053af576dc38d
ga5185cr <87967975+ga5185cr@users.noreply.github.com>
2021-07-28 15:11:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,34 +2,32 @@
 
 // self.addEventListener('fetch', function(event) {
 // });
-self.addEventListener('fetch', (e) => {})
 
-// var CACHE_VERSION = 'v1';
-// var CACHE_NAME = 'pwa-sample-caches';
-// var urlsToCache = [
-// 	'.',
-// 	'/image/neko.png',
-// 	'/css/pwa.css'
-// ];
 
-// //インストール処理
-// self.addEventListener('install', (event) => {
-// 	event.waitUntil(
-// 		caches
-// 			.open(CACHE_NAME)
-// 			.then((cache) => {
-// 				return cache.addAll(urlsToCache);
-// 			})
-// 	);
-// });
+/// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/ga5185cr.github.io/',
+];
 
-// // リソースフェッチ時のキャッシュロード処理
-// self.addEventListener('fetch', function(event) {
-// 	event.respondWith(
-// 		caches
-// 			.match(event.request)
-// 			.then(function(response) {
-// 				return response ? response : fetch(event.request);
-// 			})
-// 	);
-// });
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
